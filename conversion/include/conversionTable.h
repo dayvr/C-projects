65c0afdc91effc13a103f65026143779bc471336
Day Vazquez <dayvazquez@protonmail.com>
2022-09-24 16:41:58 +0200

    Use submodules

--- conversion/include/conversionTable.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONVERSIONTABLE_H_
-#define CONVERSIONTABLE_H_
-
-// Get a decimal number from the user and convert it to binary, hex and octal
-void conversionTable();
-
-#endif /* CONVERSIONTABLE_H_ */
